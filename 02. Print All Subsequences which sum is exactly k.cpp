eeb362ea2cc51c8ffd240c788b6e89b0ab0b653e
Souvik Sen <88578056+hr-souvik@users.noreply.github.com>
2022-08-22 00:25:42 +0530

    Update 02. Print All Subsequences which sum is exactly k.cpp
    https://practice.geeksforgeeks.org/problems/perfect-sum-problem5633/1

--- 02. Print All Subsequences which sum is exactly k.cpp
+++ 02. Print All Subsequences which sum is exactly k.cpp
@@ -34,3 +34,49 @@ int main()
    vector<int>ds;
    SubSeqFunc(arr,ds,0,0);
 }
+
+
+-------------------------------------------------------------------------------------------------------------------------------------------
+Approach : 2 (GFG)
+ #Time Limit Exsisted
+ 
+
+class Solution{
+
+   public:
+    void subsetsum(int arr[],int n, vector<int>&ds, vector<vector<int>>&ans, int i, int sum, int s)
+    {
+        if(i == n)
+        {
+            if(sum==s)
+            {
+                ans.push_back(ds);
+            
+            }
+            return;
+            
+        }
+        ds.push_back(arr[i]);
+        subsetsum(arr,n,ds,ans,i+1,sum, s+=arr[i]);
+        ds.pop_back();
+        subsetsum(arr,n,ds,ans,i+1,sum, s-=arr[i]);
+        
+    }
+    
+	public:
+	int perfectSum(int arr[], int n, int sum)
+	{
+        // Your code goes here
+        vector<vector<int>>ans;
+        vector<int>ds;
+        int index=0;
+        int s=0;
+        subsetsum(arr,n,ds,ans,index,sum, s);
+        
+        return ans.size();
+        
+	}
+	
+	  
+};
+
